0120a64653215844aa1a004abba1ed48b4a66f91
Lukia56 <kky56saqqe@gmail.com>
2026-08-24 16:57:00 +0900

    プレイヤー移動・ジャンプ処理実装

--- Assets/Osawa/Player.cs
+++ Assets/Osawa/Player.cs
@@ -1,48 +1,59 @@
+using TMPro;
+using Unity.VisualScripting;
+using UnityEditor.U2D.Animation;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class Player : MonoBehaviour
 {
     [SerializeField]
-    private Vector3 _velocity;
+    private Vector3 _force;
+
+    private bool _isJumping;
 
     private Rigidbody2D _myRigidbody = null;
 
     private InputAction _moveInput = null;
     private InputAction _jumpInput = null;
 
+    [SerializeField]
+    private LayerMask _groundLayerMask;
+
     private const float _MOVE_FORCE = 10.0f;
     private const float _JUMP_FORCE = 300.0f;
+
+    private const float _JUMP_CANCEL_THRESHOLD = 20.0f;
 
     private void Start()
     {
         _myRigidbody = GetComponent<Rigidbody2D>();
-        
+
         _moveInput = InputSystem.actions.FindAction("Move");
         _jumpInput = InputSystem.actions.FindAction("Jump");
     }
 
     private void FixedUpdate()
     {
-        _myRigidbody.AddForce(_velocity);
-        _velocity = Vector3.zero;
+        _myRigidbody.AddForce(_force);
+        _force = Vector3.zero;
     }
 
     private void Update()
     {
-        _velocity.x += _moveInput.ReadValue<Vector2>().x * _MOVE_FORCE;
+        _force.x = _moveInput.ReadValue<Vector2>().x * _MOVE_FORCE;
 
-        if (_jumpInput.WasPerformedThisFrame())
+        if (_jumpInput.WasPerformedThisFrame() && CheckGround())
         {
-            _velocity.y = _JUMP_FORCE;
+            JumpUp(_JUMP_FORCE);
+
+            _isJumping = true;
         }
-    }
 
-    private void OnCollisionEnter(Collision collision)
-    {
-        _velocity = Vector3.zero;
-
-        
+        if (_jumpInput.WasReleasedThisFrame() && _isJumping)
+        {
+            _myRigidbody.linearVelocityY = 0.0f;
+            _force.y = _JUMP_CANCEL_THRESHOLD;
+        }
     }
 
     public void Dead()
@@ -52,11 +63,19 @@ public class Player : MonoBehaviour
 
     public void JumpUp(float force)
     {
-
+        _myRigidbody.linearVelocityY = 0.0f;
+        _force.y = force;
     }
 
     public void RechargeDash()
     {
 
+    }
+
+    bool CheckGround()
+    {
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.down, 1.0f, _groundLayerMask);
+        
+        return hit.collider != null;
     }
 }
