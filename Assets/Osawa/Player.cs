e4b956a1cf2c95dc5cb201652eb0b2b8695564f0
Lukia56 <kky56saqqe@gmail.com>
2026-08-25 17:16:13 +0900

    プレイヤーダッシュ 修正前

--- Assets/Osawa/Player.cs
+++ Assets/Osawa/Player.cs
@@ -25,13 +25,15 @@ public class Player : MonoBehaviour
     [SerializeField]
     private LayerMask _groundLayerMask;
 
-    private const float _MOVE_FORCE = 200.0f;
+    private const float _MOVE_FORCE = 100.0f;
     private const float _JUMP_FORCE = 300.0f;
+
+    private const float _MIN_VELOCITY_Y = -400.0f;
 
     private const float _JUMP_CANCEL_THRESHOLD = 20.0f;
 
     private const int _MAX_DASH_NUM = 1;
-    private const float _DASH_FORCE = 500.0f;
+    private const float _DASH_FORCE = 200.0f;
     private const float _DASH_TIME = 0.35f;
 
     private void Start()
@@ -40,18 +42,52 @@ public class Player : MonoBehaviour
 
         _moveInput = InputSystem.actions.FindAction("Move");
         _jumpInput = InputSystem.actions.FindAction("Jump");
+        _dashInput = InputSystem.actions.FindAction("Sprint");
     }
 
     private void FixedUpdate()
     {
-        _myRigidbody.AddForce(_force);
-        _force = Vector2.zero;
+        Debug.Log(Mathf.Abs(_force.x));
+        Debug.Log(Mathf.Abs(_force.y / 100.0f) + 1.0f);
+        _myRigidbody.linearVelocityX += (_force.x * (Mathf.Abs(_force.y / 300.0f) + 1.0f)) / _myRigidbody.mass * Time.fixedDeltaTime;
+        _myRigidbody.linearVelocityY += _force.y / _myRigidbody.mass * Time.fixedDeltaTime;
     }
 
     private void Update()
     {
-        _force.x = _moveInput.ReadValue<Vector2>().x * _MOVE_FORCE;
+        Vector2 moveDir = _moveInput.ReadValue<Vector2>();
 
+        if (!IsDashing())
+        {
+            _force.x = _MOVE_FORCE * moveDir.x;
+        }
+
+        if (CheckGround())
+        {
+            _isJumping = false;
+
+            RechargeDash();
+
+            //if (_force.y < 0.0f)
+            //{
+            //    _force.y = 0.0f;
+            //}
+        }
+        else
+        {
+            if (!IsDashing())
+                _force.y -= 9.81f;
+        }
+
+        JumpProcess();
+
+        DashProcess();
+
+        _force.y = Mathf.Max(_force.y, _MIN_VELOCITY_Y);
+    }
+
+    private void JumpProcess()
+    {
         if (_jumpInput.WasPerformedThisFrame() && CheckGround())
         {
             SetForce(_JUMP_FORCE, Vector2.up);
@@ -63,7 +99,37 @@ public class Player : MonoBehaviour
         {
             _myRigidbody.linearVelocityY = 0.0f;
             _force.y = _JUMP_CANCEL_THRESHOLD;
+            _isJumping = false;
         }
+    }
+
+    private void DashProcess()
+    {
+        Vector2 moveDir = _moveInput.ReadValue<Vector2>();
+
+        if (_dashInput.WasPerformedThisFrame() && moveDir != Vector2.zero && CanDash())
+        {
+            SetForce(_DASH_FORCE, moveDir);
+
+            _dashCount--;
+
+            _dashTimer = _DASH_TIME;
+        }
+
+        if (IsDashing())
+        {
+            _dashTimer -= Time.deltaTime;
+        }
+    }
+
+    private bool CanDash()
+    {
+        return _dashCount > 0;
+    }
+
+    private bool IsDashing()
+    {
+        return _dashTimer > 0.0f;
     }
 
     public void Dead()
@@ -75,7 +141,6 @@ public class Player : MonoBehaviour
     {
         Assert.AreApproximatelyEqual(1.0f, normal.sqrMagnitude);
 
-        _myRigidbody.linearVelocityY = 0.0f;
         _force = normal * force;
 
         Debug.Log("Player // SetForce called");
