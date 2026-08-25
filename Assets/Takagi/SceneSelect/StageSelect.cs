15d184dc3eb32949a7a1cdafde73cd7df1df7d17
kana-bunbun <gaomuke186@gmail.com>
2026-08-25 13:56:48 +0900

    コメント追加

--- Assets/Takagi/SceneSelect/StageSelect.cs
+++ Assets/Takagi/SceneSelect/StageSelect.cs
@@ -1,10 +1,13 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+/// <summary>
+/// ステージ選択をするクラス
+/// シングルトンで管理する
+/// </summary>
 public class StageSelect
 {
     private static StageSelect _instance = null;
-
     public static StageSelect instance
     {
         get
@@ -13,20 +16,29 @@ public class StageSelect
             return _instance;
         }
     }
-
+    /// <summary>
+    /// 指定IDのステージのシーンを読み込む
+    /// </summary>
+    /// <param name="stageID"></param>
     public void LoadStage(int stageID)
     {
-        if (stageID < 0 || stageID >= SceneName._SCENE_STAGE.Length)
+        // IDが不正値なら処理しない
+        if (stageID < 0 || stageID >= SceneName.SCENE_STAGE.Length)
         {
             return;
         }
-        string sceneName= SceneName._SCENE_STAGE[stageID];
+        // 指定IDのシーン名を取得
+        string sceneName= SceneName.SCENE_STAGE[stageID];
+        // 取得した名前のシーン名を読み込む
         SceneManager.LoadScene(sceneName);
         return;
     }
+    /// <summary>
+    /// ステージ選択シーンに遷移する
+    /// </summary>
     public void LoadStageSelect()
     {
-        SceneManager.LoadScene(SceneName._SCENE_SELECTSCENE);
+        SceneManager.LoadScene(SceneName.SCENE_SELECTSCENE);
         return;
     }
     
