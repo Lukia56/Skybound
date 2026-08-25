2b6b97a64416fe458b7936c908963dc6ccecfc3b
Lukia56 <kky56saqqe@gmail.com>
2026-08-25 15:15:52 +0900

    死亡処理 ログ出力

--- Assets/Osawa/Player.cs
+++ Assets/Osawa/Player.cs
@@ -56,10 +56,6 @@ public class Player : MonoBehaviour
 
     public void Dead()
     {
-
-
-    public void Dead()
-    {
         Debug.Log("Player // 死亡処理が呼ばれました");
     }
 
@@ -79,7 +75,7 @@ public class Player : MonoBehaviour
     bool CheckGround()
     {
         RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.down, 1.0f, _groundLayerMask);
-        
+
         return hit.collider != null;
     }
 }
