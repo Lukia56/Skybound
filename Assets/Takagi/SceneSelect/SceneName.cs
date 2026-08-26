238e48fa8609a6b33a10865c14d0973f274c0a8c
kana-bunbun <gaomuke186@gmail.com>
2026-08-26 21:54:35 +0900

    髙木ステージを1つ作成

--- Assets/Takagi/SceneSelect/SceneName.cs
+++ Assets/Takagi/SceneSelect/SceneName.cs
@@ -6,7 +6,7 @@ public class SceneName
     public static readonly string SCENE_SELECTSCENE = "takagi Scene";
     public static readonly string[] SCENE_STAGE =new string[]
     {
-        "Stage1",
+        "TakagiStage1",
         "Stage2",
         "Stage3",
     };
