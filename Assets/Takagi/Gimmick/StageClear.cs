346279125f47f08b9cf26d379320febfe7fc2cf4
kana-bunbun <gaomuke186@gmail.com>
2026-08-27 14:41:03 +0900

    ゴール時プレイヤーに対する入力を受け付けなくする

--- Assets/Takagi/Gimmick/StageClear.cs
+++ Assets/Takagi/Gimmick/StageClear.cs
@@ -3,6 +3,12 @@ using UnityEngine.SceneManagement;
 
 public class StageClear : GimmickBase
 {
+    public override void ToCharacterAction(CharacterBase character, eHitType hitType)
+    {
+        if (hitType != eHitType.Enter) return;
+        // プレイヤーの入力操作を受け付けないようにする
+        character.OnReachGoal();
+    }
     public override void OtherAction(eHitType hitType)
     {
         if (hitType != eHitType.Enter) return;
