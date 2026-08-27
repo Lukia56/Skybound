5d07ac1b4ff252cf9ffdda1cc9a2805251d68d3d
kana-bunbun <gaomuke186@gmail.com>
2026-08-27 14:09:09 +0900

    ステージシーンの名前

--- Assets/Takagi/SceneSelect/SceneName.cs
+++ Assets/Takagi/SceneSelect/SceneName.cs
@@ -6,6 +6,9 @@ public class SceneName
     public static readonly string SCENE_SELECTSCENE = "takagi Scene";
     public static readonly string[] SCENE_STAGE =new string[]
     {
+        "Stage1",
+        "Stage2",
+        "Stage3",
         "TakagiStage1",
         "TakagiStage2",
         "TakagiStage3",
