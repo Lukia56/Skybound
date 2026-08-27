5d0ea3328577bfa95e8bcfce27311d0aa3bea692
kana-bunbun <gaomuke186@gmail.com>
2026-08-27 12:43:52 +0900

    コメント追加

--- Assets/Takagi/Gimmick/Spike.cs
+++ Assets/Takagi/Gimmick/Spike.cs
@@ -3,23 +3,25 @@ using UnityEngine.SceneManagement;
 
 public class Spike:GimmickBase
 {
+    // トゲのサウンドのID
+    private const int _SOUND_ID_SPIKE_ACTION = 0;
     public override void ToCharacterAction(CharacterBase character, eHitType hitType)
     {
         // 当たった瞬間でなければ処理しない
         if (hitType != eHitType.Enter) return;
         Debug.Log("ギミック発動 : トゲ " + hitType);
         if (character == null) return;
+        // キャラクターの死亡処理
         character.Dead();
         if (!character.IsPlayer()) return;
-        SoundManager.instance.PlaySE(0,Restart);
-    }
-    public override void OtherAction(eHitType hitType)
-    {
-        if (hitType != eHitType.Enter) return;
 
+        // キャラクターがプレイヤーなら死亡サウンドを再生
+        // 再生後の処理を渡す
+        SoundManager.instance.PlaySE(_SOUND_ID_SPIKE_ACTION,Restart);
     }
     private void Restart()
     {
+        // 同じシーンを再生する
         StageSceneManager.instance.LoadCurrentStage();
 
     }
